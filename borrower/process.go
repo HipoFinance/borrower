f4d2c63a150b52c2b854b699be538ceba57f1162
Behrang Norouzinia <behrangn@gmail.com>
2023-08-14 12:42:34 +0330

    Improve waiting for next round when there is no participation

--- borrower/process.go
+++ borrower/process.go
@@ -52,6 +52,17 @@ func Process() (wait time.Duration) {
 		participationsList = participations.All()
 	}
 
+	if len(participationsList) == 0 {
+		t := participateSince + 60
+		if uint32(time.Now().Unix()) > t {
+			t = nextRoundSince
+		}
+		next := time.Until(time.Unix(int64(t), 0))
+		if wait == 0 || wait > next {
+			wait = next
+		}
+	}
+
 	for _, kv := range participationsList {
 		roundSince := uint32(kv.Key.BeginParse().MustLoadUInt(32))
 		participation := LoadParticipation(kv.Value)
